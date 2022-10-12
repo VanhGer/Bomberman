6ff7280beba9342013840dab78b3a266453bfaa1
Phạm Gia Việt Anh <vietanhpg2003@gmail.com>
2022-10-12 07:05:06 +0700

    test

--- src/uet/oop/bomberman/enemies/Enemies.java
+++ src/uet/oop/bomberman/enemies/Enemies.java
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
-package uet.oop.bomberman.enemies;public class Enemies {
-}
-=======
 package uet.oop.bomberman.enemies;
 
 import javafx.scene.image.Image;
+import uet.oop.bomberman.CommonFunc;
 import uet.oop.bomberman.entities.DynamicEntity;
 
 import java.util.ArrayList;
@@ -12,8 +9,68 @@ import java.util.Collections;
 import java.util.List;
 
 public abstract class Enemies extends DynamicEntity {
+
+    public boolean Dead;
+    public int deadAnimation;
+
     public Enemies(int x, int y, Image img) {
         super(x, y, img);
+        deadAnimation = 0;
+        Dead = false;
+    }
+
+    public boolean isDead() {
+        return Dead;
+    }
+
+    public void setDead(boolean dead) {
+        Dead = dead;
+    }
+
+    public boolean isDone() {
+        return isDead() && deadAnimation >= 40;
+    }
+
+    public void check_Dead() {
+        if (check_explosing()) {
+            setDead(true);
+            deadAnimation = 0;
+            return;
+        }
+    }
+
+    @Override
+    public void update() {
+        if (isMoving() && !isDead()) {
+            int nxt_x = getX();
+            int nxt_y = getY();
+
+            if (getDir() == 1) { // UP
+                nxt_y -= getSpeed();
+            } else if (getDir() == 2) { // DOWN
+                nxt_y += getSpeed();
+            }else if (getDir() == 3) { // LEFT
+                nxt_x -= getSpeed();
+            } else if (getDir() == 4) { // RIGHT
+                nxt_x += getSpeed();
+            }
+            if (!check_colliding(nxt_x, nxt_y)) {
+                change_coordinates(nxt_x, nxt_y);
+            }
+            else {
+                autoChangeDir(getX(), getY());
+            }
+            animation++;
+            if (animation == CommonFunc.Period) animation = 0;
+        }
+        if (!isDead()) {
+            check_Dead();
+        }  else {
+            deadAnimation++;
+        }
+        if (isDead() || isMoving()) {
+            change_img();
+        }
     }
 
     public void autoChangeDir(int x, int y) {
@@ -44,4 +101,3 @@ public abstract class Enemies extends DynamicEntity {
 
     }
 }
->>>>>>> origin
--- src/uet/oop/bomberman/enemies/Oneal.java
+++ src/uet/oop/bomberman/enemies/Oneal.java
@@ -14,37 +14,21 @@ public class Oneal extends Enemies  {
         setSpeed(1);
     }
 
-    @Override
-    public void update() {
-        if (isMoving()) {
-            int nxt_x = getX();
-            int nxt_y = getY();
-            if (getDir() == 1) { // UP
-                nxt_y -= getSpeed();
-            } else if (getDir() == 2) { // DOWN
-                nxt_y += getSpeed();
-            }else if (getDir() == 3) { // LEFT
-                nxt_x -= getSpeed();
-            } else if (getDir() == 4) { // RIGHT
-                nxt_x += getSpeed();
-            }
-            if (!check_colliding(nxt_x, nxt_y)) {
-                change_coordinates(nxt_x, nxt_y);
-            }
-            else {
-                // change speed when colliding.
-                Random d = new Random();
-                setSpeed(d.nextInt(2) + 1);
-                autoChangeDir(getX(), getY());
-            }
-            animation++;
-            if (animation == CommonFunc.Period) animation = 0;
-            change_img();
-        }
-    }
 
     @Override
     public void change_img() {
+        if (isDead()) {
+            setImg(
+                    Sprite.movingSprite(
+                                    Sprite.oneal_dead,
+                                    Sprite.oneal_dead,
+                                    Sprite.oneal_dead,
+                                    deadAnimation,
+                                    CommonFunc.Period)
+                            .getFxImage());
+            return;
+        }
+
         /** left. */
         if (getDir() == 1 || getDir() == 3) {
             setImg(
@@ -70,8 +54,4 @@ public class Oneal extends Enemies  {
         }
     }
 
-    @Override
-    public boolean isDone() {
-        return false;
-    }
 }
